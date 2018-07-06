10d7dbb1b38debe72e094e781c60ab401c5dfb10
Aldrick Handinata <aldrickhandinata1@gmail.com>
2018-07-06 19:37:32 +0700

    Menambahkan fitur SumX

--- Program.cs
+++ Program.cs
@@ -24,8 +24,14 @@ namespace ConsoleApp1
                         n -= x;
                     }
                     return res;
-                } else
+                }
+                else if(x > 1000 || x < -1000 || n > 1000 || n < -1000){
+                    Console.WriteLine("Maaf angka terlalu besar");
+                    return res;
+                }
+                else
                 {
+
                     while (x >= n)
                     {
                         temp += x;
@@ -40,15 +46,19 @@ namespace ConsoleApp1
 
         static bool checkNum(int x)
         {
-            if (x >= 0) return true;
+            if (x >= 0 && x <= 1000) return true;
             return false;
         }
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Aplikasi SumX ");
+            Console.WriteLine("=============");
+            Console.WriteLine();
             Console.WriteLine("sumX(3, 10): " + (sumX(3, 10) == 18));
             Console.WriteLine("sumX(1, 10): " + (sumX(1, 10) == 55));
             Console.WriteLine("sumX(0, 10): " + (sumX(0, 10) == 0));
+            Console.WriteLine("SumX(1500, 100) : " + (sumX(1500,100) ==0));
             Console.WriteLine("sumX(-1, 10): " + (sumX(-1, 10) == 0));
             Console.WriteLine("sumX(-1, -10): " + (sumX(-1, -10) == -55));
         }
